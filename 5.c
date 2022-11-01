0b8ffc2a85444cca23de8d5cdaaac2ec3f6f87ae
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-11-01 14:08:30 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int ary[3][3]={{1,2,3},{4,5,6},{7,8,9}},i,j,sum=0;
+    while(j<3){
+        for(i=0;i<3;i++){
+            if(i==j)
+            sum=sum+ary[i][j];
+        }
+        j++;
+    }
+    printf("%d",sum);
+    return 0;
+}
